6374b0b8b9c2570f089deea5849301bfadb5d85c
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2020-04-07 23:35:05 +0300

    fix: fix characters deleting

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const dataObject = {
   lowerCaseEn: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592;',
-    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
     'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
     'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '&#9650;', '/', 'Shift',
     'Ctrl', 'Win', 'Alt', '&emsp;', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
   upperCaseEn: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592;',
-    'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del',
+    'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\',
     'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter',
     'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '&#9650;', '/', 'Shift',
     'Ctrl', 'Win', 'Alt', '&emsp;', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
   lowerCaseRu: ['ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', '&#8592;',
-    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'Del',
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/',
     'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
     'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '&#9650;', 'Shift',
     'Ctrl', 'Win', 'Alt', '&emsp;', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
   upperCaseRu: ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592;',
-    'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del',
+    'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\',
     'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
     'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '&#9650;', 'Shift',
     'Ctrl', 'Win', 'Alt', '&emsp;', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
   codes: ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8',
     'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY',
-    'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS',
+    'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS',
     'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft',
     'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'ArrowUp', 'Slash',
     'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft',
@@ -116,7 +116,6 @@ const printOnClick = (keyboard, textfield) => {
 
     switch (event.target.innerText) {
       case ('←'):
-      case ('Del'):
         textfield.value = textfield.value.slice(0, -1);
         break;
       case ('Enter'): textfield.value += '\n';
@@ -184,7 +183,6 @@ const keyStyleHandler = (elem) => {
 };
 
 const printOnKeypress = () => {
-  
   document.addEventListener('keydown', (event) => {
     event.preventDefault();
     if (event.code === 'CapsLock') {
@@ -217,21 +215,19 @@ const printOnKeypress = () => {
       }
     }
 
-    if(event.code === 'AltLeft' || event.code === 'AltRight' || event.code === 'Tab') {
+    if (event.code === 'AltLeft' || event.code === 'AltRight' || event.code === 'Tab') {
       event.preventDefault();
     }
 
 
-    switch(document.getElementById(event.code).innerText) {
+
+    switch (document.getElementById(event.code).innerText) {
       case 'CapsLock':
-      case 'Alt':
-      case 'Ctrl':
       case 'Shift':
+      case 'Ctrl':
+      case 'Alt':
       case 'Win':
         document.querySelector('TEXTAREA').value += '';
-        break;
-      case '←':
-        document.querySelector('TEXTAREA').value = document.querySelector('TEXTAREA').value.slice(0, -1);
         break;
       case 'Enter':
         document.querySelector('TEXTAREA').value += '\n';
@@ -239,16 +235,16 @@ const printOnKeypress = () => {
       case 'Tab':
         document.querySelector('TEXTAREA').value += '\t';
         break;
+      case '←':
+        document.querySelector('TEXTAREA').value = document.querySelector('TEXTAREA').value.slice(0, -1);
+        break;
       default:
         document.querySelector('TEXTAREA').value += document.getElementById(event.code).innerText;
-    };
+    }
 
     document.getElementById(event.code).classList.add('active');
     document.querySelector('TEXTAREA').focus();
   });
-
-
-
 
   document.addEventListener('keyup', (event) => {
     document.querySelectorAll('DIV').forEach((item) => {
